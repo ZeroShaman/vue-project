88179804f4f5ea498b4921db54f4174aad1ec654
Андрей Казаковцев <kav@MCT-NEW.local>
2018-05-15 13:55:10 +0300

    Урок 4. Добавление ссылок и адаптация.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <v-app>
-    <v-navigation-drawer app temporary></v-navigation-drawer>
+    <v-navigation-drawer app temporary v-model="drawer">
+      <v-list>
+        <v-list-tile v-for="link of links" :key="link.title" :to="link.url">
+          <v-list-tile-action>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="link.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
     <v-toolbar app dark color="primary">
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-side-icon class="hidden-md-and-up"
+        @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Vue Application</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>Link One</v-btn>
+        <v-btn flat
+          v-for="link of links" :key="link.title" :to="link.url">
+          <v-icon left>{{link.icon}}</v-icon>
+          {{link.title}}
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
@@ -16,5 +32,18 @@
 </template>
 
 <script>
-export default {}
+  export default {
+    data () {
+      return {
+        drawer: false,
+        links: [
+          {title: 'Login', icon: 'lock', url: '/login'},
+          {title: 'Registration', icon: 'face', url: '/registration'},
+          {title: 'Orders', icon: 'bookmark_border', url: '/orders'},
+          {title: 'New ad', icon: 'note_add', url: '/new'},
+          {title: 'My ads', icon: 'list', url: '/list'}
+        ]
+      }
+    }
+  }
 </script>
