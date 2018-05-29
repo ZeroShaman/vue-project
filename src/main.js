349c7ccadc1fff6cb35285616c1362caa2cf8c9a
Andrey Kazakovtsev <gnoms113@gmail.com>
2018-05-29 21:56:02 +0300

    Урок 15. Настройка firebase.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
+import * as fb from 'firebase'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
@@ -15,5 +16,15 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyDmiyDP4eIjve84Qh-YimyyTN8FE_4rbgs',
+      authDomain: 'vivchar-vinograd.firebaseapp.com',
+      databaseURL: 'https://vivchar-vinograd.firebaseio.com',
+      projectId: 'vivchar-vinograd',
+      storageBucket: 'vivchar-vinograd.appspot.com',
+      messagingSenderId: '1075454688412'
+    })
+  }
 })
